a3bf142f185c13f704f1028d9649b8cf7b5516c3
User <zagrkostya19952017@gmail.com>
2026-08-26 22:10:28 +0300

    Clean up whitespace in WiFi manager header file

--- lib/wifi_manager/wifi_manager.h
+++ lib/wifi_manager/wifi_manager.h
@@ -2,16 +2,9 @@
 #define WIFI_MANAGER_H
 #include <Arduino.h>
 #include <WiFi.h>
-
-
 void initWiFi();
-
 bool loadSavedWifi(String &ssid, String &password);
-
 bool saveWifi(const String &ssid, const String &password);
-
 void clearSavedWifi();
-
 bool isWifiConfigured();
-
 #endif
